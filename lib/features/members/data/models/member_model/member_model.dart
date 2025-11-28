3674283b19836b96f7f0d420a0a6c1b360997a43
Belal Karem <belalkarem41@gmail.com~>
2025-11-28 23:07:33 +0200

    create member model

---
+++ lib/features/members/data/models/member_model/member_model.dart
@@ -0,0 +1,14 @@
+class MemberModel {
+  final String id, name, phone;
+  final String start_data, end_data, absence, attendance;
+
+  MemberModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.start_data,
+    required this.end_data,
+    required this.absence,
+    required this.attendance,
+  });
+}
