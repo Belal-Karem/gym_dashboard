e48ba2a6eb90865d7c33a5f54d37653521a783e4
Belal Karem <belalkarem41@gmail.com~>
2025-11-29 15:27:37 +0200

    create constants file and Member Model .fromjson from meber model

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+const kMemberCollections = 'member';
+const kid = 'id';
+const kname = 'name';
+const kPhone = 'phone';
+const kattendance = 'attendance';
+const kabsence = 'absence';
+const kStartdate = 'Start date';
+const kenddate = 'End date';
--- lib/features/members/data/models/member_model/member_model.dart
+++ lib/features/members/data/models/member_model/member_model.dart
@@ -1,14 +1,27 @@
+import 'package:power_gym/constants.dart';
+
 class MemberModel {
   final String id, name, phone;
-  final String start_data, end_data, absence, attendance;
+  final String startdata, enddata, absence, attendance;
 
   MemberModel({
     required this.id,
     required this.name,
     required this.phone,
-    required this.start_data,
-    required this.end_data,
+    required this.startdata,
+    required this.enddata,
     required this.absence,
     required this.attendance,
   });
+  factory MemberModel.fromjson(jsondata) {
+    return MemberModel(
+      id: jsondata[kid],
+      name: jsondata[kname],
+      phone: jsondata[kPhone],
+      startdata: jsondata[kStartdate],
+      enddata: jsondata[kenddate],
+      absence: jsondata[kabsence],
+      attendance: jsondata[kattendance],
+    );
+  }
 }
